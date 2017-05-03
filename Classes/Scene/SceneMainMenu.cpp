463cd3e08f4c0728f83d1124d489f70ee25bd41b
jie.liu <b527902714@gmail.com>
2017-05-03 23:07:48 +0800

    Revert "add background"
    This reverts commit b6e540fd382d1b3abaa4fb6b515552aab68f83fd.

--- Classes/Scene/SceneMainMenu.cpp
+++ Classes/Scene/SceneMainMenu.cpp
@@ -1,8 +1,7 @@
 #include "Utilities/Utilities.h"
 #include "Scene/SceneManager.h"
 #include "Scene/SceneMainMenu.h"
-#include "Scene/Layer/BallonLayer.h"
-#include "Scene/Layer/BgLayer.h"
+#include "Scene/Layer/touchpull.h"
 
 
 //#include "GameObject/GameObjectManager/GameObjectManager.h"
@@ -47,13 +46,9 @@ namespace DestoryColor
 
     void SceneMainMenu::onEnter()
     {
-		
         SceneBase::onEnter();
-		auto ballon = BallonLayer::create();
-        this->addChild(ballon,1); 
-
-		auto bg = BgLayer::create();
-		this->addChild(bg,1);
+        auto layer = TouchPull::create();
+        this->addChild(layer); 
     }
 
     void SceneMainMenu::onExit()
